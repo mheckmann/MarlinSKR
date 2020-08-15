44f5deed7adcf5cb8bfd1097c1fbc8339da3c6b8
Matt Heckmann <183231+mheckmann@users.noreply.github.com>
2020-08-15 08:59:43 -0400

    setting zoffset to 0, prefer to set it up via gcode

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -999,7 +999,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 43, 6, -1.74 }
+#define NOZZLE_TO_PROBE_OFFSET { 43, 6, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
